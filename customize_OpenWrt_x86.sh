056c813d1d24ab59eb155600287e3079860329b6
riches2020cn <141565770+riches2020cn@users.noreply.github.com>
2024-03-15 12:17:47 +0800

    Update customize_OpenWrt_x86.sh

--- customize_OpenWrt_x86.sh
+++ customize_OpenWrt_x86.sh
@@ -10,7 +10,7 @@
 sed -i 's/192.168.1.1/192.168.9.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. 修改时区
-sed -i 's/timezone='UTC'/timezone='CST-8'/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/UTC/CST-8/g' openwrt/package/base-files/files/bin/config_generate
 
 #3. 修改 x86 内核版本
 #sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' openwrt/target/linux/x86/Makefile
